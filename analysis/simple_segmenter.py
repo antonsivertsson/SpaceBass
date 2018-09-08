737256b4c64caa28698edfeddd75ad8f27d579d0
Josef Grahn <josef.grahn@volumental.com>
2018-09-08 20:26:06 +0200

    add very image segmenter, based on a pixel-wise GMM on spectral data

---
+++ analysis/simple_segmenter.py
@@ -0,0 +1,24 @@
+import numpy as np
+from sklearn import mixture
+
+
+class PixelGMM:
+
+    def __init__(self, n_comp=6):
+        self.model = mixture.GaussianMixture(n_components=n_comp, covariance_type='full')
+    
+    def fit(self, images):
+        n_channels = images[0].shape[-1]
+        # Flatten the pixels.
+        n_pix = 1
+        for dim in images[0].shape[:-1]:
+            n_pix *= dim
+        pixels = np.stack(images).reshape((n_pix*len(images), n_channels))
+        print(pixels.shape)
+        self.model.fit(pixels)
+    
+    def segment(self, image):
+        (nr, nc, n_channels) = image.shape
+        pixels = image.reshape((nr*nc, n_channels))
+        labels = self.model.predict(pixels)
+        return labels.reshape((nr, nc))
