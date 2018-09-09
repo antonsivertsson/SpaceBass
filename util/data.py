d916fc5983d65624293345c3b12548962f8cb4c3
Josef Grahn <josef.grahn@volumental.com>
2018-09-09 07:13:48 +0200

    load image files recursively

--- util/data.py
+++ util/data.py
@@ -51,7 +51,7 @@ def load_image_set(path, with_labels=False):
     imgs = []
     names = []
     labels = []
-    for fn in sorted(glob.glob("%s/*.npy" % path)):
+    for fn in sorted(glob.iglob("%s/**/*.npy" % path, recursive=True)):
         with open(fn, "rb") as f:
             imgs.append(np.load(f))
             names.append(fn)
