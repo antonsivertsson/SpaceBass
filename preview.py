173330c67080cdf958c952be15fe2a4e17d9dbbd
Josef Grahn <josef.grahn@volumental.com>
2018-09-09 04:38:51 +0200

    add plotting helpers and improve preview

--- preview.py
+++ preview.py
@@ -2,14 +2,9 @@ import sys
 import json
 import numpy as np
 from client import sentinel2
-from util.plot import optimize_raw_for_display
+from util import plot
 from config import *
 
-def show(image):
-    import matplotlib.pyplot as plt
-    import matplotlib.image as mpimg
-    imgplot = plt.imshow(image)
-    plt.show()
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
@@ -25,15 +20,8 @@ if __name__ == "__main__":
     client = sentinel2.Client(api_key=API_KEY)
     series = client.fetch_series(lng=coords[0], lat=coords[1])
     frame = list(series.frames())[frame_index]
-    img = frame.fetch_rgba()
-    show(img)
-    raw = frame.fetch_data()
     print(frame.date)
-    opt = np.dstack(
-        [
-            optimize_raw_for_display(raw[:, :, 3]),
-            optimize_raw_for_display(raw[:, :, 2]),
-            optimize_raw_for_display(raw[:, :, 1]),
-        ]
-    )
-    show(opt)
+    img = frame.fetch_rgba()
+    raw = frame.fetch_data()
+    opt = plot.optimize_raw_for_display(raw, [3, 2, 1])
+    plot.show_all([img, opt])
--- util/plot.py
+++ util/plot.py
@@ -1,4 +1,25 @@
 from skimage import exposure
+import numpy as np
 
-def optimize_raw_for_display(raw):
-    return exposure.equalize_adapthist(raw, clip_limit=0.03)
+def optimize_raw_for_display(raw, channels=None):
+    if len(raw.shape) == 2 or raw.shape[2] == 1:
+        return exposure.equalize_adapthist(raw, clip_limit=0.03)
+    else:
+        (nr, nc, d) = raw.shape
+        if channels is None:
+            channels = range(d)
+        return np.dstack([optimize_raw_for_display(raw[:, :, ch]) for ch in channels])
+
+def show(image):
+    import matplotlib.pyplot as plt
+    import matplotlib.image as mpimg
+    imgplot = plt.imshow(image)
+    plt.show()
+
+def show_all(images):
+    import matplotlib.pyplot as plt
+    import matplotlib.image as mpimg
+    for i in range(len(images)):
+        plt.subplot((len(images) + 3) // 4, min(4, len(images)), i+1)
+        imgplot = plt.imshow(images[i])
+    plt.show()
